c5e041a605b5bb34c570cfe135a1d29175918685
JPPDT <johnpaul.detorres@cvsu.edu.ph>
2024-11-05 09:06:51 +0800

    Updated

--- assets/js/bootsnav.js
+++ assets/js/bootsnav.js
@@ -659,6 +659,27 @@ $('.video-section').on('translated.owl.carousel', function (event) {
                 });
 });
     
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    const offset = -currentSlide * 100;
+    document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+}
+
+function changeSlide(direction) {
+    showSlide(currentSlide + direction);
+}
+
+// Initial display
+showSlide(currentSlide);
 
 }(jQuery));
 
